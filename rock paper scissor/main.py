fc3ca5779e3c0eda149698d6d5d950a3f6c2bdaa
quazifahimulalam <itsusor@gmail.com>
2025-09-09 22:19:45 +0600

    Add files via upload

--- rock paper scissor/main.py
+++ rock paper scissor/main.py
@@ -109,3 +109,44 @@ class rpsgame():
             self.pl_score += 1
             
         return self.pc_score
+    
+    def image_reset(self):
+        self.screen.blit(self.text, (330, 0))
+        self.text = self.font.render( " ", True , (0 , 0 , 0))
+        self.screen.blit(self.bg, (0, 0))
+        self.screen.blit(self.r_btn, (20, 500))
+        self.screen.blit(self.p_btn, (330, 500))
+        self.screen.blit(self.s_btn, (640, 500))
+        pass
+    
+    def game_loop(self):
+        run = True
+        clock = pygame.time.Clock()
+        rps_game = rpsgame()
+        
+        while run:
+            pygame.display.update()
+            self.screen.blit(self.text, (330, 0))
+            
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.rock_btn.clicked(30) or self.paper_btn.clicked(340) or self.scissors_btn.clicked(640):
+                        rps_game.image_reset()
+                        rps_game.player()
+                        rps_game.computer()
+                        
+                        self.pl_score += rps_game.pl_score_cache()
+                        self.pc_score += rps_game.pc_score_cache()
+                        self.text = self.font.render(f"{self.pl_score} : {self.pc_score}", True , (255 , 255 , 255))
+                        
+            pygame.display.flip()
+            clock.tick(30)
+        pygame.quit()
+        
+if __name__ == "__main__":
+    game = rpsgame()
+    game.game_loop()
